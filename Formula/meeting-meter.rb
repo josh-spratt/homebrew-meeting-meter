102bfce8e3c7654271a59f96ee7bfcb0be08c94c
Josh Spratt <josh.spratt@ramseysolutions.com>
2026-02-02 22:00:40 -0600

    meeting-meter 0.4.0 (#4)
    * meeting-meter 0.4.0
    
    * Update meeting-meter.rb

--- Formula/meeting-meter.rb
+++ Formula/meeting-meter.rb
@@ -1,12 +1,12 @@
 class MeetingMeter < Formula
   desc "CLI to measure and analyze meeting time"
   homepage "https://github.com/josh-spratt/meeting-meter"
-  version "0.3.0"
+  version "0.4.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/josh-spratt/meeting-meter/releases/download/v0.3.0/meeting-meter-macOS"
-      sha256 "42dd934f955f13b0a9989d6a8dbebfbbac57eb1babd950b6a5c66219c508729d"
+      url "https://github.com/josh-spratt/meeting-meter/releases/download/v0.4.0/meeting-meter-macOS"
+      sha256 "618cf262403f979627b61304a4ba2e64c2ae6f6e1861a45162f9a765d96a0e7f"
     end
   end
 
