5071fd54e1d7bc4dba4a5b1c187f776de53b40a7
josh-spratt <61611179+josh-spratt@users.noreply.github.com>
2026-02-04 04:31:20 +0000

    meeting-meter 0.4.1

--- Formula/meeting-meter.rb
+++ Formula/meeting-meter.rb
@@ -5,7 +5,7 @@ class MeetingMeter < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/josh-spratt/meeting-meter/releases/download/v0.4.0/meeting-meter-macOS"
+      url "https://github.com/josh-spratt/meeting-meter/releases/download/v0.4.1/meeting-meter-macOS"
       sha256 "618cf262403f979627b61304a4ba2e64c2ae6f6e1861a45162f9a765d96a0e7f"
     end
   end
